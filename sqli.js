a24886bb9633af88d6cdadce09caa27aca7a0438
aadviboutique <84778005+aadviboutique@users.noreply.github.com>
2021-06-07 18:38:09 +0530

    Update sqli.js

--- sqli.js
+++ sqli.js
@@ -4,7 +4,7 @@ const db = require('./db');
 const router = express.Router();
 
 router.get('/email', (req, res) => {
-  db.query('SELECT email FROM users WHERE id = ' + req.query.id)
+  db.query('SELECT email FROM users WHERE id = ' + req.params.id)
     .then((record) => {
       // logical flow
       res.send(record[0]);
